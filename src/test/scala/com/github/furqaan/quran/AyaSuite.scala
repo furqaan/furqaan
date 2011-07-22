0e134541a70cc2900bad27eec71b869d9201e095
Jamal Aziz <jamalsaepulaziz@gmail.com>
2011-07-22 15:42:44 +0700

    Change test description in AyaSuite.

--- src/test/scala/com/github/furqaan/quran/AyaSuite.scala
+++ src/test/scala/com/github/furqaan/quran/AyaSuite.scala
@@ -16,13 +16,13 @@ class AyaSuite extends FunSuite with MustMatchers {
 	aya.text must not equal (null)
   }
   
-  test("first aya must equal بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ (bismillaahi-r-rahmaani-r-rahiim)") {
+  test("first aya text must equal بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ (bismillaahi-r-rahmaani-r-rahiim)") {
     val f = fixture
 	val aya = new Aya(((f.source \ "sura")(0) \ "aya")(0))
 	aya.text must equal ("بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ")
   }
   
-  test("last aya must equal مِنَ الْجِنَّةِ وَالنَّاسِ (mina-l-jinnati wa-n-naasi)") {
+  test("last aya text must equal مِنَ الْجِنَّةِ وَالنَّاسِ (mina-l-jinnati wa-n-naasi)") {
     val f = fixture
 	val aya = new Aya(((f.source \ "sura")(113) \ "aya")(5))
 	aya.text must equal ("مِنَ الْجِنَّةِ وَالنَّاسِ")
