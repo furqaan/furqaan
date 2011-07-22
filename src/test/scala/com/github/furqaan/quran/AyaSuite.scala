3fb6e2fd57c52a3b56e24af79abf3d028c4a4de0
Jamal Aziz <jamalsaepulaziz@gmail.com>
2011-07-22 15:01:13 +0700

    Add test suite for Aya.

---
+++ src/test/scala/com/github/furqaan/quran/AyaSuite.scala
@@ -0,0 +1,30 @@
+package com.github.furqaan.quran
+
+import scala.xml._
+import org.scalatest.FunSuite
+import org.scalatest.matchers.MustMatchers
+
+class AyaSuite extends FunSuite with MustMatchers {
+  def fixture =
+    new {
+	  val source = XML.load(getClass().getResource("/quran-simple-enhanced.xml"))
+	}
+  
+  test("aya must contain text") {
+    val f = fixture
+	val aya = new Aya(((f.source \ "sura")(0) \ "aya")(0))
+	aya.text must not equal (null)
+  }
+  
+  test("first aya must equal بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ (bismillaahi-r-rahmaani-r-rahiim)") {
+    val f = fixture
+	val aya = new Aya(((f.source \ "sura")(0) \ "aya")(0))
+	aya.text must equal ("بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ")
+  }
+  
+  test("last aya must equal مِنَ الْجِنَّةِ وَالنَّاسِ (mina-l-jinnati wa-n-naasi)") {
+    val f = fixture
+	val aya = new Aya(((f.source \ "sura")(113) \ "aya")(5))
+	aya.text must equal ("مِنَ الْجِنَّةِ وَالنَّاسِ")
+  }
+}
